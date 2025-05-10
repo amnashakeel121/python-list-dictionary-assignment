c85f150155bf6dfd7c04559054d3cc24a759c3df
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-10 06:30:17 +0500

    Add files via upload

---
+++ list_ dictionary_prictice.py
@@ -0,0 +1,16 @@
+fruits=["apple", "watermelon","grapes","strawberry", "dates"] 
+print("Fruit list:", fruits)
+print("First fruit:", fruits[0])
+print("third fruit:", fruits[2])
+print("Fifth fruit:",fruits[4])
+fruits.append("orange")
+print("Updated fruits list:", fruits)
+# Dictionary Example
+student={
+    "name":"umer",
+    "age":17,
+    "grade":"10th"
+}
+print ("Student Info:", student)
+print("Name:",student["name"])
+)
